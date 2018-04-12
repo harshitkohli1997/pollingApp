cbfeb77d4c24f2d2ebc7a9a42c7c8a5045ed91d0
harshit kohli <harshit.kohli1997@gmail.com>
2018-04-13 03:08:54 +0530

    adding routes

---
+++ routes/poll.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router()
+const Pusher = require('pusher');
+
+var pusher = new Pusher({
+    appId: '508507',
+    key: '313043d5093a413702e2',
+    secret: 'b7e9b25f4cb7f2c83f27',
+    cluster: 'ap2',
+    encrypted: true
+  });
+
+router.get('/', (req,res) => {
+    res.send('poll');
+});
+
+router.post('/', (req,res) => {
+    pusher.trigger('os-pole', 'os-vote', {
+        points:1 ,
+        os:req.bosy.os
+      }) 
+      res.json({success :true, message :'thanks for voting'})
+});
+
+module.exports = router;
+
