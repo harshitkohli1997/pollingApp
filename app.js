0f3608fcf86496573c7463db5c47860302ec3522
harshit kohli <harshit.kohli1997@gmail.com>
2018-04-13 03:07:31 +0530

    add pusher

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+const poll = require('./routes/poll');
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,6 +13,8 @@ app.use(bodyParser.json());
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//routes handling
+app.use('/poll',poll);
 port = 8000;
 
 app.listen(port, () => {
